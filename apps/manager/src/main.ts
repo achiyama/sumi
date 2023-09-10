3ea75b29de0c8231a3219b8cc6851fd2545b5692
Kohei Akiyama <kohei.akiyama@outlook.jp>
2023-09-11 01:45:25 +0900

    修正

--- apps/manager/src/main.ts
+++ apps/manager/src/main.ts
@@ -3,6 +3,7 @@ import { ManagerModule } from './manager.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ManagerModule);
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
--- apps/skytree/src/main.ts
+++ apps/skytree/src/main.ts
@@ -3,6 +3,7 @@ import { SkytreeModule } from './skytree.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(SkytreeModule);
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
